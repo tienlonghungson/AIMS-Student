4c9d340738adb8ae424b2288d9b4ba396be63589
tienlonghungson <nguyentienlong0203hungson@gmail.com>
2021-12-09 16:22:34 +0700

    refactoer API

--- src/utils/API.java
+++ src/utils/API.java
@@ -20,20 +20,47 @@ import java.util.logging.Logger;
 import entity.payment.CreditCard;
 import entity.payment.PaymentTransaction;
 
+/**
+ * provides methods helping wth sending request to server and handle returned values
+ * Date 9/12/2021
+ * @author tienlonghungson
+ */
 public class API {
 
+	/**
+	 * format of date value
+	 */
 	public static DateFormat DATE_FORMATER = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+	/**
+	 * attribute to log information to console
+	 */
 	private static Logger LOGGER = Utils.getLogger(Utils.class.getName());
 
+	/**
+	 * method to call GET API
+	 * @param url link to requested server
+	 * @param token hash code to verify user
+	 * @return response : response from server (String type)
+	 * @throws Exception
+	 */
 	public static String get(String url, String token) throws Exception {
-		LOGGER.info("Request URL: " + url + "\n");
-		URL line_api_url = new URL(url);
-		HttpURLConnection conn = (HttpURLConnection) line_api_url.openConnection();
-		conn.setDoInput(true);
-		conn.setDoOutput(true);
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty("Content-Type", "application/json");
-		conn.setRequestProperty("Authorization", "Bearer " + token);
+
+		// set up
+		HttpURLConnection conn = getHttpURLConnection(url,"GET", token);
+
+		// read returned data from server
+		StringBuilder response = getStringBuilder(conn);
+		return response.substring(0, response.length() - 1);
+	}
+
+	/**
+	 * read returned data from server
+	 * @param conn connection to server
+	 * @return response : response from server
+	 * @throws IOException
+	 */
+	private static StringBuilder getStringBuilder(HttpURLConnection conn) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 		String inputLine;
 		StringBuilder respone = new StringBuilder(); // ising StringBuilder for the sake of memory and performance
@@ -42,15 +69,44 @@ public class API {
 		respone.append(inputLine + "\n");
 		in.close();
 		LOGGER.info("Respone Info: " + respone.substring(0, respone.length() - 1).toString());
-		return respone.substring(0, respone.length() - 1).toString();
+		return respone;
+	}
+
+	/**
+	 * set up connection to server
+	 * @param url link to requested server
+	 * @param method : API protocol
+	 * @param token: hash code to verify user
+	 * @return connection
+	 * @throws IOException
+	 */
+	private static HttpURLConnection getHttpURLConnection(String url,String method, String token) throws IOException {
+		LOGGER.info("Request URL: " + url + "\n");
+		URL line_api_url = new URL(url);
+		HttpURLConnection conn = (HttpURLConnection) line_api_url.openConnection();
+		conn.setDoInput(true);
+		conn.setDoOutput(true);
+		conn.setRequestMethod(method);
+		conn.setRequestProperty("Content-Type", "application/json");
+		conn.setRequestProperty("Authorization", "Bearer " + token);
+		return conn;
 	}
 
 	int var;
 
+	/**
+	 * method to call POST API
+	 * @param url link to requested server
+	 * @param data data that is brought to server (json type)
+	 * @return response : response from server (string type)
+	 * @throws IOException
+	 */
 	public static String post(String url, String data
 //			, String token
 	) throws IOException {
 		allowMethods("PATCH");
+
+		// set up
 		URL line_api_url = new URL(url);
 		String payload = data;
 		LOGGER.info("Request Info:\nRequest URL: " + url + "\n" + "Payload Data: " + payload + "\n");
@@ -60,24 +116,22 @@ public class API {
 		conn.setRequestMethod("PATCH");
 		conn.setRequestProperty("Content-Type", "application/json");
 //		conn.setRequestProperty("Authorization", "Bearer " + token);
+
+		// send data
 		Writer writer = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
 		writer.write(payload);
 		writer.close();
-		BufferedReader in;
-		String inputLine;
-		if (conn.getResponseCode() / 100 == 2) {
-			in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		} else {
-			in = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-		}
-		StringBuilder response = new StringBuilder();
-		while ((inputLine = in.readLine()) != null)
-			response.append(inputLine);
-		in.close();
-		LOGGER.info("Respone Info: " + response.toString());
+
+		// read returned data from server
+		StringBuilder response = getStringBuilder(conn);
 		return response.toString();
 	}
 
+	/**
+	 * method allows calling to different API : PUT, PATCH
+	 * @deprecated since Java 11
+	 * @param methods methods need to be allowed (PUT, PATCH,...)
+	 */
 	private static void allowMethods(String... methods) {
 		try {
 			Field methodsField = HttpURLConnection.class.getDeclaredField("methods");
